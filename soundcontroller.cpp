1cb4eefd23f4b7550f1b9bf48aa75a6a63a833f7
ChardAllen <allenrichi@hotmail.co.uk>
2015-04-16 01:20:54 +0100

    Everything working upto sound controller

---
+++ soundcontroller.cpp
@@ -0,0 +1,27 @@
+#include "soundcontroller.h"
+
+//ADD SOUNDS TO BE PLAYED HERE
+//CALL FUNCTIONS WHEN NECCESSARY
+
+/*
+bool SoundController::Template()
+{
+bool result;
+
+result = LoadWaveFile("Sounds/Filename.wav", &m_secondaryBuffer1);
+if(!result)
+{
+return false;
+}
+
+result = PlayWaveFile();
+if(!result)
+{
+return false;
+}
+
+return true;
+
+
+}
+*/
---
+++ soundcontroller.h
@@ -0,0 +1,16 @@
+#include "soundclass.h"
+
+class SoundController
+{
+private:
+	SoundClass* m_sounds;
+
+public:
+	SoundController();
+	~SoundController();
+
+	//ADD SOUNDS TO BE PLAYED HERE
+	//CALL FUNCTIONS WHEN NECCESSARY
+
+	//bool Template();
+};
